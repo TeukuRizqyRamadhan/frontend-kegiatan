09a56dd2f16e2524179734515a97db1fa88093cd
unknown <kikidotid@gmail.com>
2024-06-11 21:40:05 +0700

    frontend kegiatan

--- src/store/kegiatanAsync.ts
+++ src/store/kegiatanAsync.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Kegiatan } from '../types';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://kegiatan-be.vercel.app',
 });
 
 export const fetchKegiatan = createAsyncThunk('kegiatan/fetchKegiatan', async () => {
